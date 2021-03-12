08560b4a060e042274038243a2e34e9d094b7608
fujishig2 <fujishig@ualberta.ca>
2021-03-12 15:35:33 -0700

    added comments and fixed some things

--- server.py
+++ server.py
@@ -75,8 +75,6 @@ users = Users()
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
 def flask_post_json():
-    '''Ah the joys of frameworks! They do so much work for you
-       that they get in the way of sane operation!'''
     if (request.json != None):
         return request.json
     elif (request.data != None and request.data.decode("utf8") != u''):
@@ -84,93 +82,131 @@ def flask_post_json():
     else:
         return json.loads(request.form.keys()[0])
 
+# Home page. This is the usable draw board
 @app.route("/")
 def index():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
     return flask.send_from_directory('static','index.html')
 
+# Entity API (Post/Put only). This is the API which handles entity calls.
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
     try:
+        # Post handler. Create a new entity
         if request.method == "POST":
             req_json = flask_post_json()
+            # Check to see if the entity already exists. If it does exist, 
+            # create a new entity with name X(length).
             if myWorld.get(entity) != {}:
                 entity = "X" + str(len(myWorld.world()))
             myWorld.set(entity, req_json)
-            return app.response_class(response=json.dumps(myWorld.get(entity)), mimetype='application/json')
-        elif request.method == "PUT":
-            req_json = flask_post_json()
-            for key in req_json:
-                myWorld.update(entity, key, req_json[key])
+            # Return the entity using a direct call to the world to ensure that we are sending
+            # the worlds data.
             return app.response_class(response=json.dumps(myWorld.get(entity)), mimetype='application/json')
 
+        # Put handler. Update an existing entity
+        elif request.method == "PUT":
+            req_json = flask_post_json()
+            # Update the entitys attributes given in the update request
+            for key in req_json:
+                myWorld.update(entity, key, req_json[key])
+            # Return the entity using a direct call to the world to ensure that we are sending
+            # the worlds data.
+            return app.response_class(response=json.dumps(myWorld.get(entity)), mimetype='application/json')
+
+    # Error handler
     except:
         return "Cannot update entity.", 400
     return None
 
+# World API (Post/Get only). This API handles all calls to the world.
 @app.route("/world", methods=['POST','GET'])    
 def world():
+    # Get handler. Returns the current world
     if request.method == 'GET':
-        print(request)
         return app.response_class(response=json.dumps(myWorld.world()), mimetype='application/json')
+
+    # Post handler. Sets the world to be the world given in the post body
     elif request.method == 'POST':
         try:
             req_json = flask_post_json()
+            # Clear the world
             world.clear()
+            # Set the world to be the world found in the post request
             for key in req_json:
                 world.set(key, req_json[key])
+            # Return the new world
             return app.response_class(response=json.dumps(myWorld.world()), mimetype='application/json')
+        # Error handler
         except:
             return "Cannot update world.", 400
         
     else:
         return "Method not handled.", 500
 
+# Entity Get handler. Gets a given entity's information
 @app.route("/entity/<entity>")    
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
     if request.method == 'GET':
         return app.response_class(response=json.dumps(myWorld.get(entity)), mimetype='application/json')
     return "Method not handled.", 500
 
+# Clear Post/Get API. Clears the world of all its contents.
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
     if request.method == 'GET' or request.method == 'POST':
         try:
+            # Clear the world
             myWorld.clear()
+            # Return the cleared world
             return app.response_class(response=json.dumps(myWorld.world()), mimetype='application/json')
         except:
             return "Cannot clear world.", 400
     return "Method not handled.", 500
 
+# Listener GET API. Gets a new listener value which a client can use to
+# get the worlds information based off of the last call it made to this listner.
 @app.route("/listener", methods=["GET"])
 def get_listener():
+    # Global values which are being changed during this function call
     global listener
     global users
     if request.method == 'GET':
+        # Setup the response
         response = {"listener": listener}
+        # Create a new user at the current listener value with a blank world
         users.setWorld(str(listener), World())
+        # Incriment listner value
         listener += 1
+        # Return listner value which the user can call to get updates
         return app.response_class(response=json.dumps(response), mimetype='application/json') 
     else:
         return "Method not handled.", 500
 
+# Listener/<listener> GET API. Gets the worlds values that may have changed since the
+# last time this API was called.
 @app.route("/listener/<listener>", methods=["GET"])
 def get_diff(listener):
+    # Global users is the only global value being changed
     global users
     if request.method == 'GET':
         try:
+            # Setup the response, world and the users world
             response = {}
             world = myWorld.world()
             userWorld = users.getWorld(listener)
-            # print(users.getWorld(listener).world())
+            userWorldValues = userWorld.world()
+
+
+            # Check to see if there have been any changes to the world since the last time user world was called.
+            # If there are changes, update the user world to be the same as the servers world.
             for key in world:
-                if key not in userWorld.world():
+                if key not in userWorldValues or userWorldValues[key] != world[key]:
                     response[key] = world[key]
                     userWorld.set(key, world[key])
-            for key in userWorld.world():
+            # Check to see if the users world is somehow ahead of the servers world. If it is, send a refresh message.
+            # This particular check works for when the world is cleared.
+            for key in userWorldValues:
                 if key not in world:
                     response["message"] = "refresh"
             return app.response_class(response=json.dumps(response), mimetype='application/json') 
